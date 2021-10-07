0c7525e7cc0da2b4d51be818987a650a10f58c8b
KooBH <ninie4409@sogang.ac.kr>
2021-10-07 13:16:43 +0900

    fix typo

--- KWidgetASR.cmake
+++ KWidgetASR.cmake
@@ -2,9 +2,9 @@
 #        CMake file for using KWidgetASR   as submodule        #
 ################################################################
 
-file(COPY ${MAKE_CURRENT_LIST_DIR}/move_contents_to_bin/libcurl.dll
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/move_contents_to_bin/libcurl.dll
      DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/bin)
-file(COPY ${MAKE_CURRENT_LIST_DIR}/move_contents_to_bin/libcurl.lib
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/move_contents_to_bin/libcurl.lib
      DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/bin)
 
 list(APPEND SRC_Qt_MOC
